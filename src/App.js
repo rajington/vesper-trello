67434f7adb70f07ead91eedb77db9d8d3a35cc06
Rajarshi Nigam <raj@nigam.com>
2016-08-25 13:26:55 -0400

    basic bootstrap layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1>vesper-trello</h1>
+              <p>Import your Vesper notes into Trello.</p>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            Connect to Trello
+          </Col>
+          <Col xs={4}>
+            Select "Vesper Export ƒ" Folder
+          </Col>
+          <Col xs={4}>
+            Upload Cards
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
