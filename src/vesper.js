9e9f2853310d8c3539e2ad6bdec016935066bbcc
Rajarshi Nigam <raj@nigam.com>
2016-08-25 17:56:42 -0400

    vesper note parsing

--- src/vesper.js
+++ src/vesper.js
@@ -8,33 +8,76 @@ const getPictures = files =>
         map.set(picture.webkitRelativePath, picture)
     , new Map());
 
-const parseNote = (text, pictures) => text;
+const parseNote = async (file, pictures) => {
+  const text = await fileHelper.readText(file);
+
+  const lines = text.split('\n');
+
+  const note = {};
+
+  // active or archived
+  if (fileHelper.isArchived(file)) {
+    note.archived = true;
+  }
+
+  // the first line is the title
+  note.title = lines.shift();
+
+  // unless it's an untitled photo
+  if (note.title === '') {
+    note.title = 'Untitled photo'; // what it says in Vesper
+  }
+
+  // unused lines
+  lines.pop(); // separator
+  lines.pop(); // modified date
+  lines.pop(); // created date
+  lines.pop(); // separator
+
+  // every note has the tags field
+  const tagString = lines.pop().replace(/^Tags: /, '');
+  // but not all of them have tags
+  if (tagString) {
+    note.tags = tagString.split(', ');
+  }
+  lines.pop(); // separator
+
+  // check for picture field
+  if (lines.length >= 2 && lines[lines.length - 1].startsWith('Picture: ')) {
+    const pictureLine = lines.pop();
+    const pictureName = pictureLine.replace(/^Picture: /, '');
+    const pictureFolder = fileHelper.getFolder(note.archived);
+    const picturePath = pictureFolder + pictureName;
+    note.picture = pictures.get(picturePath);
+    lines.pop(); // separator
+  }
+
+  // remaining text
+  note.text = lines.join('\n');
+
+  return note;
+};
 
 const createNotes = (files, pictures) =>
   Promise.all(
     files
       .filter(fileHelper.isNote)
       .map(
-        async file => {
-          const text = await fileHelper.readText(file);
-          return parseNote(text, pictures);
-        }
+        async file => await parseNote(file, pictures)
       )
   );
 
-export default {
-  parseFiles(files) {
-    const filteredFiles = Array.from(files)
-      .filter(fileHelper.isVesperExport)
-      .filter(fileHelper.isNotDSStore);
+export const parseFiles = (files) => {
+  const filteredFiles = Array.from(files)
+    .filter(fileHelper.isVesperExport)
+    .filter(fileHelper.isNotDSStore);
 
-    if(filteredFiles.length === 0) {
-      throw new Error('Could not find any notes.');
-    }
-
-    const pictures = getPictures(filteredFiles);
-    const notes = createNotes(filteredFiles, pictures);
-
-    return notes;
+  if(filteredFiles.length === 0) {
+    throw new Error('Could not find any notes.');
   }
-}
+
+  const pictures = getPictures(filteredFiles);
+  const notes = createNotes(filteredFiles, pictures);
+
+  return notes;
+};
