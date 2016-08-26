b6edbc5bccd5f9700ff5f9ad30241c1b907372f3
Rajarshi Nigam <raj@nigam.com>
2016-08-25 22:39:11 -0400

    additional branding and directions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 
+import logo from './logo.svg';
+import './App.css';
+
 import { authorized } from './trello';
 
 import TrelloStep from './TrelloStep';
@@ -25,8 +28,30 @@ export default class App extends Component {
         <Row>
           <Col>
             <Jumbotron>
-              <h1>vesper-trello</h1>
-              <p>Import your Vesper notes into Trello.</p>
+              <Grid>
+                <Row>
+                  <Col xs={6}>
+                    <h1>vesper-trello</h1>
+                    <p>Import your Vesper notes into Trello.</p>
+                    This tool requires:
+                    <ul>
+                      <li>
+                        <a href="https://www.google.com/chrome/browser/desktop/index.html">Desktop Chrome</a>
+                      </li>
+                      <li>
+                        <a href="https://itunes.apple.com/us/app/vesper/id655895325">Vesper notes</a> exported to a folder.
+                      </li>
+                      <li>
+                        A <a href="https://trello.com/">Trello account</a>
+                      </li>
+                    </ul>
+                    More information available on <a href="https://medium.com/p/vesper-trello-168732c63d97">medium</a> and <a href="https://github.com/rajington/vesper-trello">github</a>.
+                  </Col>
+                  <Col xs={6}>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                  </Col>
+                </Row>
+              </Grid>
             </Jumbotron>
           </Col>
         </Row>
