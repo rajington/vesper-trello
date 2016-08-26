ccb7a61e01ff9c578ab1c7a49d80eb47dc2b0558
Rajarshi Nigam <raj@nigam.com>
2016-08-25 21:40:33 -0400

    show import progress

--- src/ImportStep.js
+++ src/ImportStep.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ProgressBar, Panel } from 'react-bootstrap';
 
 import Step from './Step';
 import { importNotes } from './trello';
 
 export default class ImportStep extends Component {
   state = {
-    progress: 0
+    progress: 0,
+    url: '',
   }
-  
-  handleImport = () => {
-    importNotes(this.props.notes, this.updateProgress);
+
+  handleImport = async () => {
+    const url = await importNotes(this.props.notes, this.updateProgress);
+    this.setState({url});
   }
 
   updateProgress = progress => {
@@ -18,9 +20,26 @@ export default class ImportStep extends Component {
   }
 
   render() {
-    const { active }  = this.props;
+    const { active, notes }  = this.props;
+    const { progress, url } = this.state;
+    let infoPanel;
+    if(progress) {
+      const total = notes.length;
+      const incomplete = progress !== total;
+      const message = incomplete ? 'Uploading...' : (<a href={url}>{url}</a>);
+      infoPanel = (
+        <Panel header={message} bsStyle={incomplete ? 'warning' : 'success'}>
+          <ProgressBar
+            now={progress / total * 100}
+            label={`${progress}/${total} cards`}
+            active={incomplete}
+            striped
+          />
+        </Panel>
+      );
+    }
     return (
-      <Step>
+      <Step info={infoPanel}>
         <Button disabled={!active} onClick={this.handleImport}>
           Upload Cards {this.state.progress}
         </Button>
