20d2a98872a36223341a11377faa3c851531f73d
Rajarshi Nigam <raj@nigam.com>
2016-08-25 20:55:47 -0400

    add progress state

--- src/ImportStep.js
+++ src/ImportStep.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
-import Step from './Step';
 
-export default ({active}) => (
-  <Step>
-    <Button disabled={!active}>
-      Upload Cards
-    </Button>
-  </Step>
-);
+import Step from './Step';
+import { importNotes } from './trello';
+
+export default class ImportStep extends Component {
+  state = {
+    progress: 0
+  }
+  
+  handleImport = () => {
+    importNotes(this.props.notes, this.updateProgress);
+  }
+
+  updateProgress = progress => {
+    this.setState({ progress });
+  }
+
+  render() {
+    const { active }  = this.props;
+    return (
+      <Step>
+        <Button disabled={!active} onClick={this.handleImport}>
+          Upload Cards {this.state.progress}
+        </Button>
+      </Step>
+    );
+  }
+}
